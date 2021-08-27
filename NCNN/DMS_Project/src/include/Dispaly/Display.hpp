753fc92b6df9d88ddb458b9ad5ae8521feb052af
karljiang118440 <32283913+karljiang118440@users.noreply.github.com>
2021-08-27 15:29:39 +0800

    Display.hpp

---
+++ NCNN/DMS_Project/src/include/Dispaly/Display.hpp
@@ -0,0 +1,26 @@
+/*  add by karl --20210816
+1). add nanodet hand into program 
+
+*/
+#include <iostream>
+#include <stdio.h>
+#include <array>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include "stdlib.h"
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+
+int display(cv::Mat tmp1);
+
+
